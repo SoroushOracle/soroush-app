e6c594ff8a38013711fff466b3f89c94e9b5e94c
Soroush Oracle <soroushoracle1@gmail.com>
2020-08-15 15:48:50 +0430

    add facebook script

--- index.php
+++ index.php
@@ -7,5 +7,15 @@
     <h1>This is a heading</h1>
     <p>This is a paragraph.</p>
   </body>
-
+ <script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '789575811872186',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v1.0'
+    });
+  };
+ </script>
+ <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
 </html> 
