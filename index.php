2c236065e6e73c947def93f15ed9c9491be23e54
Soroush Oracle <soroushoracle1@gmail.com>
2020-08-15 16:14:33 +0430

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 
   <body>
     <h1>Facebook login</h1>
+   <div class="fb-login-button" data-size="small" data-button-type="login_with" data-layout="rounded" data-auto-logout-link="false" data-use-continue-as="false" data-width=""></div>
     <div id="fb-root"></div>
       <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v8.0&appId=789575811872186&autoLogAppEvents=1" nonce="jTcu4vlN"></script>
   </body>
