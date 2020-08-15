c469ca92779976277c1e11d51076d0a18e0414f9
Soroush Oracle <soroushoracle1@gmail.com>
2020-08-15 16:13:09 +0430

    add login dacebook button

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
   </head>
 
   <body>
-    <h1>This is a heading</h1>
-    <p>This is a paragraph.</p>
+    <h1>Facebook login</h1>
+    <div id="fb-root"></div>
+      <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v8.0&appId=789575811872186&autoLogAppEvents=1" nonce="jTcu4vlN"></script>
   </body>
  <script>
   window.fbAsyncInit = function() {
